4676917a7a1537555fa9319de15028a981a75d58
smccrindle <iainmccrindle@gmail.com>
2026-09-25 10:20:57 -0400

    Add project files.

---
+++ RetroVideoGameStore/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetroVideoGameStore.Models;
+
+namespace RetroVideoGameStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = new List<Category>();
+            for (int i = 1; i <= 10; i++)
+            {
+                categories.Add(new Category { Id = i, Name = "Category " + i });
+            }
+            return View(categories);
+        }
+        public IActionResult Browse(string categoryName)
+        {
+            ViewBag.CategoryName = categoryName;
+            return View();
+        }
+    }
+}
---
+++ RetroVideoGameStore/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetroVideoGameStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+    }
+}
